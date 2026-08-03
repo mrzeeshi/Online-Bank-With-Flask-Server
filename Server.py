cca4fa8e7b68d6aeef858d54ed0570caee0ab06b
Zeeshan Haider <z.haider1504@gmail.com>
2026-08-03 19:00:01 +0500

    day two of making

--- Server.py
+++ Server.py
@@ -1,7 +1,7 @@
-from flask import Flask
-app=Flask(__name__)
-@app.route('/')
+from flask import Flask,render_template,request,redirect
+app=Flask(__name__,template_folder='.')
+@app.route('/signup')
 def home():
-    return 'hello world'
+    return render_template('Signup.html' , error='this is the error')
 if __name__=='__main__':
     app.run(debug=True)
